41732eb9e07472c82d6fdb673033c6b3b04da632
ALUNO <ALUNO@senaiescola.fsa>
2024-03-01 10:56:09 -0300

    first commit

---
+++ index-script.js
@@ -0,0 +1,43 @@
+//
+/*var a = 3;
+var b = 5;
+
+document.write(a == b);
+
+document.write(b != 10);
+
+document.write(a === 5);
+
+document.write(a <= b);
+
+document.write(b == "5");
+*/
+
+/*
+var a = 3;
+
+var b = 5;
+
+document.write(!(a==b));
+
+document.write((b != 10) && (a <= b));
+
+document.write((b== "5") || (a===b));
+*/
+
+/*
+
+var a = 3;
+var b = 5;
+
+if( a < b){
+  document.write(" a < b ");
+}else if(a > b){
+  document.write(" a > b ");
+}else{
+  document.write("a = b");
+}
+*/
+
+/* */
+
